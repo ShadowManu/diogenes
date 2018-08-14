26943bb4cb7b9a4bd2456a7f00a3680d1406511b
Manuel Pacheco <manuelalejandropm@gmail.com>
2018-08-14 01:26:36 -0400

    rework map in newmapgen scene with tile collection scriptable object

--- Assets/Scripts/Map/Map.cs
+++ Assets/Scripts/Map/Map.cs
@@ -1,27 +1,26 @@
 using UnityEngine;
 using System.Linq;
 
-[RequireComponent(typeof(MeshFilter))]
-public class Map: MonoBehaviour {
-  public int size = 5;
+public class Map : MonoBehaviour
+{
+  public GameObject[][] grid;
+  public TileCollection tileCollection;
 
-  // private Tile[][] grid;
-
-  void Start() {
-
+  void Start()
+  {
     BuildGrid();
   }
 
-  private void BuildGrid() {
-    grid = Enumerable.Range(0, size).Select(i =>
-      Enumerable.Range(0, size).Select(j =>
-        new Tile(i, j)
-      ).ToArray()
+  private void BuildGrid()
+  {
+    grid = Enumerable.Range(0, 4).Select(i =>
+      Enumerable.Range(0, 4).Select(j =>
+      {
+        int index = (i * 4) + j;
+        GameObject tile = tileCollection.tiles[index];
+        Vector3 position = new Vector3(i, 0, j);
+        return Instantiate<GameObject>(tile, position, tile.transform.rotation, transform);
+      }).ToArray()
     ).ToArray();
-
-    Instantiate(null, null, null, null);
-
-    GetComponent<MeshFilter>().mesh = m;
   }
-
 }
